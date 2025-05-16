196652532a179530cec771f14756f32db6638526
Максим <142081655+tgmaksim@users.noreply.github.com>
2025-05-16 21:17:29 +0600

    2.8.0 (107)
    1. Удаление временных файлов режима призрака при перезапуске

--- reload.py
+++ reload.py
@@ -1,8 +1,10 @@
 import os
+import re
 import aiohttp
 import asyncio
 
 from dotenv import load_dotenv
+from core import www_path, time_now
 
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
@@ -14,6 +16,14 @@ process_id = os.environ['ProcessIdMaksogram']
 
 
 async def reload():
+    for file in os.listdir(www_path("posts")):
+        old = int(re.split(r'\.|-', file)[3])
+        if old + 24*60*60 <= time_now().timestamp():
+            os.remove(www_path(f"posts/{file}"))
+    for file in os.listdir(www_path("stories")):
+        old = int(re.split(r'\.|-', file)[3])
+        if old + 24*60*60 <= time_now().timestamp():
+            os.remove(www_path(f"stories/{file}"))
     async with aiohttp.ClientSession() as session:
         async with session.post("https://panel.netangels.ru/api/gateway/token/",
                                 data={"api_key": api_key}) as response:
