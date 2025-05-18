393ef93a154b1e109574068dd0b8b4ea3953c69d
Максим <142081655+tgmaksim@users.noreply.github.com>
2025-05-18 14:15:08 +0600

    2.8.0 (110)
    1. Изменение синтаксиса

--- core.py
+++ core.py
@@ -366,8 +366,8 @@ async def send_email_message(to: str, subject: str, text: str, *, subtype: str =
 
 
 class Variables:
-    version = "2.7"
-    version_string = "2.8.0 (109)"
+    version = "2.8"
+    version_string = "2.8.0 (110)"
     fee = 150
 
     TelegramApplicationId = int(os.environ['TelegramApplicationId'])
